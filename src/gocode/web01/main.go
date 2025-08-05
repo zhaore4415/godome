531f4a5e2ca4454163447c79ec33f3bdb343cfbe
赵欢 <zhaore4415@163.com>
2025-08-05 23:42:07 +0800

    web修复

--- src/gocode/web01/main.go
+++ src/gocode/web01/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 type PageVariables struct {
@@ -33,27 +34,28 @@ func main() {
 			Items:   []string{"Item 1", "Item 2", "Item 3"},
 		}
 		u2 := User{
-			Name:	 "myzhaohuan",	
+			Name:   "myzhaohuan",
 			Gender: "男",
 			Age:    18,
 		}
-	
+
 		// 传递多个变量到模板
 		c.HTML(200, "index.tmpl", gin.H{
 			"Title":   data.Title,
 			"Message": data.Message,
-			"Items":   data.Items,	
-			"u2": u2,
+			"Items":   data.Items,
+			"u2":      u2,
 		})
 
 		// 使用 HTML 方法渲染模板并返回
 		c.HTML(200, "index.tmpl", data)
 
-		// 如果有多个模板文件，可以使用下面的方式指定模板
-		// c.HTML(200, "other.tmpl", data)
+	})
+	// 如果有多个模板文件，可以使用下面的方式指定模板
+	// c.HTML(200, "other.tmpl", data)
 	// 启动服务器
-	log.Println("服务器已启动，请访问: http://localhost:8080")
-	err := r.Run(":8080") // 默认监听 8080 端口
+	log.Println("服务器已启动，请访问: http://localhost:8081")
+	err := r.Run(":8081") // 默认监听 8080 端口
 	if err != nil {
 		log.Fatal("启动失败:", err)
 	}
